87d9586ec2ff9009e6753436ed7cc03203673c6a
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-06-19 15:24:36 +0600

    for

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let mut number = 3;
-    while number != 0 {
-        println!("{}!", number);
-        number = number -1;
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
     }
-    println!("LIFTOFF!!!");
 }
