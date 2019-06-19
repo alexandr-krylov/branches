034026140f33c85456c3b4c63cadaa2bdb0484a3
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-06-19 14:53:28 +0600

    exercise 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let number = 7;
+    let number = 3;
 
-    if number < 5 {
-        println!("condition was true");
+    if number != 0 {
+        println!("number was other then zero");
     } else {
-        println!("condition was false");
+        println!("number was zero");
     }
 }
