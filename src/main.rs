7cd9dafe958d1f5a7aec4d5c1cc1a077b25dbc9a
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-06-19 15:17:16 +0600

    while

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 fn main() {
-    let number = 5;
-
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3 or 2");
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number = number -1;
     }
+    println!("LIFTOFF!!!");
 }
